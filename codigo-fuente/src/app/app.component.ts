740b50e6188f67276f0a8e79121010bcb5d43ed3
AnderPro <fabiander_21@hotmail.es>
2020-03-14 14:34:07 -0400

    mascaras

---
+++ codigo-fuente/src/app/app.component.ts
@@ -0,0 +1,49 @@
+import {Component, OnInit} from '@angular/core';
+import {IConfig} from "ngx-mask";
+import {CurrencyMaskConfig} from "ng2-currency-mask/src/currency-mask.config";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'Máscaras ';
+  phone: number;
+  precioAuto: number = 0;
+  peso: number = 0;
+
+
+  phoneMask = ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
+
+  placaAuto = {
+    mask: [/[A-Z]/, /[A-Z]/, /[A-Z]/, '-', /\d/, /\d/, /\d/, /\d/],
+    guide: false
+  };
+
+  cedulaMask = {
+    mask: [/\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, '-', /\d/],
+    guide: false
+
+  };
+
+  configuracionMascaraPeso = {
+    prefix: '',
+    suffix: 'Kg',
+    align: 'left',
+    thousands: '.',
+    decimal: ',',
+    precision: 4
+  };
+
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+
+
+  }
+
+
+}
